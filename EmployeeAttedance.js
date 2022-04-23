f08ee84f1125a8ddddfe2b51993cc2e27c35d25f
barbadeadesh <barbadeadesh2020@email.com>
2022-04-23 08:18:08 +0530

    ADDED: Functionality foe checking the employee is present or absent

---
+++ EmployeeAttedance.js
@@ -0,0 +1,8 @@
+const is_absent = 0 
+ let empcheck = Math.floor(Math.random() * 10); ;
+ if(empcheck == is_absent) {
+     console.log("Employee is Absent");
+ }
+ else{
+     console.log("Employee is present");
+ }
